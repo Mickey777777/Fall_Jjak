55ba63e0687c81fb84253bbb0ed719d662a579e3
ianpp <ianpp2427@gmail.com>
2026-05-28 07:22:48 +0900

    Feat/UI ux (#17)
    * fix: 조작법 설명 수정
    
    * fix: 메인메뉴 문구 수정
    
    * fix: 판정 표시 위치 수정
    
    초안, 피드백 환영
    
    * fix: 콤보 위치 수정
    
    * style: 콤보 위치 설명 수정
    
    * feat: 판정에 따른 텍스트 효과 추가
    
    * fix: 폰트 수정
    
    * fix: 판정 폰트 수정

--- src/components/ControlOverlay.tsx
+++ src/components/ControlOverlay.tsx
@@ -20,42 +20,42 @@ export default function ControlOverlay() {
             </div>
             <div>
               <b>마우스 커서</b>
-              <div>커서를 움직여 점프할 방향을 조절합니다.</div>
+              <div>커서를 움직여 점프할 방향을 조절.</div>
             </div>
           </div>
           <div className="t-item">
             <div className="t-key">RMB</div>
             <div>
               <b>우클릭 유지 + 드래그</b>
-              <div>드래그한 거리만큼 점프 거리가 증가합니다.</div>
+              <div>드래그한 거리만큼 점프 거리 증가.</div>
             </div>
           </div>
           <div className="t-item">
             <div className="t-key">RMB</div>
             <div>
               <b>우클릭 놓기</b>
-              <div>설정한 방향과 거리로 점프합니다.</div>
+              <div>설정한 방향과 거리로 점프.</div>
             </div>
           </div>
           <div className="t-item">
             <div className="t-key">A / S</div>
             <div>
               <b>궤적 조절</b>
-              <div>A로 높게, S로 낮게 점프합니다.</div>
+              <div>A로 높게, S로 낮게 점프.</div>
             </div>
           </div>
           <div className="t-item">
             <div className="t-key">LMB</div>
             <div>
-              <b>파리 선택</b>
-              <div>날아다니는 파리에 커서를 두고 좌클릭합니다.</div>
+              <b>파리 사냥</b>
+              <div>날아다니는 파리에 커서를 두고 좌클릭.</div>
             </div>
           </div>
           <div className="t-item">
             <div className="t-key">ESC</div>
             <div>
               <b>일시정지</b>
-              <div>게임을 잠시 멈춥니다.</div>
+              <div>게임을 멈춤.</div>
             </div>
           </div>
         </div>
